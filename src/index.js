7310ec1cbae1845e242cb8b5f3b7140e7461372b
Junxiang <junxiang.oriint@gmail.com>
2018-04-12 12:33:45 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import $ from 'jquery';
+import axios from 'axios';
+import qs from 'qs';
+
 import { plus, minus } from './lib/math';
 
 import css from './assets/css/index.css';
@@ -40,4 +43,63 @@ window.onload = () => {
 	btnNew.onclick = () => {
 		document.getElementsByClassName('index')[0].appendChild(appendImg());
 	};
+
+	var axiosGetData = () => {
+		return new Promise((resolve, reject) => {
+			axios
+				.get('')
+				.then(function(response) {
+					//console.log(response.data);
+					resolve(response.data);
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		});
+	};
+
+	axios
+		.get('')
+		.then(function(response) {
+			console.log(response.data);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+
+	var jsonBody = {};
+
+	axios
+		.post(
+			'',
+			qs.stringify(jsonBody)
+		)
+		.then(function(response) {
+			console.log(response.data);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+
+	$.ajax({
+		processData: false,
+		type: 'POST',
+		url: '',
+		dataType: 'json',
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(jsonBody),
+		success: function(data) {
+			console.log('device control succeeded');
+		},
+		error: function() {
+			console.log('Device control failed');
+		},
+	});
+	async function get() {
+		const x = await axios.get(
+			''
+		);
+		console.log(x.data);
+	}
+	get();
 };
